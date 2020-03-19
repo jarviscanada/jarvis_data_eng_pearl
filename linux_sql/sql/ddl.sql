a59096a23fb489b503a370e131a98735cdfe7bc3
pearlkwok <pearl.kwok0308@gmail.com>
2020-03-19 18:09:12 +0000

    fix sql queries

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -1,27 +1,28 @@
 
 
 --create a `host_agent` database if not exist
+DROP DATABASE IF EXISTS host_agent;
 CREATE DATABASE host_agent; 
 
 
 --switch to `host_agent`
-\c host_agent;
+\c host_agent
 
 --create `host_info` table if not exist
 DROP TABLE IF EXISTS public.host_info CASCADE;
 CREATE TABLE PUBLIC.host_info 
       ( 
-         id               SERIAL NOT NULL, 
-         hostname         VARCHAR NOT NULL, 
-         cpu_number       INT2 NOT NULL, 
+         id               SERIAL PRIMARY KEY, 
+         hostname         VARCHAR NOT NULL UNIQUE, 
+         cpu_number       INT NOT NULL, 
          cpu_architecture VARCHAR NOT NULL, 
          cpu_model        VARCHAR NOT NULL, 
          cpu_mhz          FLOAT8 NOT NULL, 
-         l2_cache         INT4 NOT NULL, 
-         "timestamp"      TIMESTAMP NULL, 
-         total_mem        INT4 NULL, 
-         CONSTRAINT host_info_pk PRIMARY KEY (id), 
-         CONSTRAINT host_info_un UNIQUE (hostname) 
+         l2_cache         INT NOT NULL, 
+         "timestamp"      TIMESTAMP NOT NULL, 
+         total_mem        INT NOT NULL 
+         --CONSTRAINT host_info_pk PRIMARY KEY (id), 
+         --CONSTRAINT host_info_un UNIQUE (hostname) 
       ); 
 --Sample INSERT statement
 --INSERT INTO host_info (id, hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, "timestamp", total_mem) VALUES(1, 'jrvs-remote-desktop-centos7-6.us-central1-a.c.spry-framework-236416.internal', 1, 'x86_64', 'Intel(R) Xeon(R) CPU @ 2.30GHz', 2300, 256, '2019-05-29 17:49:53.000', 601324);
@@ -32,16 +33,13 @@ DROP TABLE IF EXISTS public.host_usage CASCADE;
 CREATE TABLE PUBLIC.host_usage 
       ( 
          "timestamp"    TIMESTAMP NOT NULL, 
-         host_id        SERIAL NOT NULL, 
-         memory_free    INT4 NOT NULL, 
-         cpu_idel       INT2 NOT NULL, 
-         cpu_kernel     INT2 NOT NULL, 
-         disk_io        INT4 NOT NULL, 
-         disk_available INT4 NOT NULL, 
-         CONSTRAINT host_usage_host_info_fk FOREIGN KEY (host_id) REFERENCES host_info(id) 
-      ); 
---Sample INSERT statement
---INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idel, cpu_kernel, disk_io, disk_available) VALUES('2019-05-29 15:48:47.000', 1, 123, 123, 123, 123, 123);
+         host_id        SERIAL NOT NULL REFERENCES public.host_info(id), 
+         memory_free    INT NOT NULL, 
+         cpu_idel       INT NOT NULL, 
+         cpu_kernel     INT NOT NULL, 
+         disk_io        INT NOT NULL,
+	 disk_available INT NOT NULL
+);
 
 
 
--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -3,21 +3,19 @@
 --Group hosts by CPU number and sort by their memory size in descending order(within each cpu_number group)
 SELECT
 	cpu_number, id, total_mem
-FROM	host_info
-GROUP BY cpu_number
-ORDER BY total_mem DESC
-;
+FROM	PUBLIC.host_info
+ORDER BY max(total_mem) OVER (PARTITION BY cpu_number) DESC,
+		cpu_number DESC;
 
 
 --Average used memory in percentage over 5 mins interval for each host. (used memory = total memory - free memory).
-SELECT 
-	hu.host_id, hu.hostname, 
-	TIMESTAMP WITH TIME ZONE 'epoch' + INTERVAL '1 second' * round(extract('epoch' from timestamp) / 300) * 300 as timestamp, 
-	((hi.total_mem-avg(hu.memory_free)*1024)/hi.total_mem*100) AS avg_used_mem_percentage
-FROM host_usage hu JOIN host_info hi
+SELECT
+	hu.host_id, hi.hostname,
+	ROUND(avg((hi.total_mem-hu.memory_free*1024) * 100 / hi.total_mem), 2) AS usedMemoryPercentage,
+	date_trunc('hour', hu.timestamp) + date_part('minute', hu.timestamp)::int / 5 * interval '5 min' AS fiveMinuteInterval
+FROM public.host_usage hu JOIN public.host_info hi
 ON hu.host_id=hi.id
-GROUP BY hu.host_id, round(extract('epoch' from timestamp) / 300)
-;
+GROUP BY fiveMinuteInterval, hu.host_id, hi.hostname;
 
 
 --Detect node failure. Find out when a node failed to write usage data to DB three times in a row.
