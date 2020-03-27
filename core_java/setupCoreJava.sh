98fbe5f733390e83b01a7d175e0caafdc185c893
pearlkwok <pearl.kwok0308@gmail.com>
2020-03-27 20:18:47 +0000

    create core_java project structure

---
+++ core_java/setupCoreJava.sh
@@ -0,0 +1,31 @@
+
+name_suffix="firstname"
+# cd to root dir
+#cd /home/centos/dev/jarvis_data_eng_${name_suffix}
+# create java_apps project direcotry
+#cd core_java
+#update dev branch
+git checkout develop
+git pull
+# create a sub-direcotory for each app
+mkdir grep jdbc twitter
+touch README.md
+# inti README.md
+cat > README.md << EOF
+# Core Java Apps
+This project is under development. Since this project follows the GitFlow, the final work will be merged to the master branch after Team Code Team.
+
+1. [Java Grep App](./grep)
+2. [JDBC App](./jdbc)
+3. [Twitter CLI App](./twitter)
+EOF
+
+#create empty tmp file for teach project
+#each project follows maven standard dir layout https://maven.apache.org/guides/introduction/introduction-to-the-standard-directory-layout.html
+for dir in $(ls -d */); do
+touch $dir/pom.xml;
+touch $dir/README.md;
+mkdir -p $dir/src/test/java/ca/jrvs/apps/$dir
+mkdir -p $dir/src/main/java/ca/jrvs/apps/$dir
+done
+
